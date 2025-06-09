aeeaff33eab163550d686072819067fe8eee87ab
rafahahon <rafaellapintohahon@gmail.com>
2025-06-09 08:17:18 -0300

    Código sensor de umidade

--- SensorUmidade/src/main.cpp
+++ SensorUmidade/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define PINO_ANALOGICO 34 // Conectado na saída A0 do sensor
+#define pinAnalogico 34 // saída A0 do sensor
 #define pinLed 2
 
 void setup()
@@ -13,7 +13,7 @@ void setup()
 void loop()
 {
     // Leitura analógica
-    int valorAnalogico = analogRead(PINO_ANALOGICO);
+    int valorAnalogico = analogRead(pinAnalogico);
 
     Serial.print("Nível da água: ");
     Serial.println(valorAnalogico);
@@ -21,32 +21,27 @@ void loop()
     if (valorAnalogico > 4000)
     {
         Serial.println("Terra extremamente seca");
-        digitalWrite(pinLed, HIGH);
     }
 
     else if (valorAnalogico > 3000)
     {
         Serial.println("Terra seca");
-        digitalWrite(pinLed, LOW);
     }
 
     else if (valorAnalogico > 2500)
     {
-        Serial.println("Planta necessita de agua");
-        digitalWrite(pinLed, LOW);
+        Serial.println("Planta necessita de água");
     }
 
     else if(valorAnalogico > 700)
     {
-        Serial.println("Niveis de agua estaveis");
-        digitalWrite(pinLed, LOW);
+        Serial.println("Níveis de água estáveis");
     }
 
     else if(valorAnalogico < 700)
     {
-        Serial.println("Terra extremamente umida");
-        digitalWrite(pinLed, HIGH);
+        Serial.println("Terra extremamente úmida");
     }
 
-    delay(5000); // Espera 1 segundo
+    delay(5000); 
 }
